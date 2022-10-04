e577e086ee79b9037354e77278ce4723a3c1b273
Agatha Lafaiety <72627509+agathalafaiety@users.noreply.github.com>
2022-10-04 01:23:35 -0300

    Update where.sql

--- where.sql
+++ where.sql
@@ -1,22 +1,7 @@
 SELECT coluna1,coluna2,coluna_n
 FROM tabela
-WHERE  condicao 
+WHERE  condicao
 
-/*
-
-Operador    Descrção 
-=           IGUAL
->           MAIOR QUE
-<           MENOR QUE 
->=          MAIOR QUE OU IGUAL
-<=          MENOR QUE OU IGUAL
-<>          DIFERENTE DE 
-AND         OPERADOR LOGICO E
-OR          OPERADOR LOGICO OU 
-
-*/
-
--- videoaula
 SELECT *
 FROM person.Person
 WHERE LastName = 'miller' and FirstName = 'Anna'
@@ -25,7 +10,7 @@ SELECT *
 FROM production.Product
 WHERE color = 'blue' or color = 'black'
 
-T *
+SELECT *
 FROM production.Product
 WHERE ListPrice > 1500 and ListPrice < 5000
 

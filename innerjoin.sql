549456d249dd2de4f11264c59763cdb640317a88
Agatha Lafaiety <agatha.lafaietyy@gmail.com>
2023-01-12 07:13:36 -0300

    commit

---
+++ innerjoin.sql
@@ -0,0 +1,28 @@
+
+SELECT c.Cliente, C.Nome, E.Rua, E.Cidade
+FROM Client C 
+INNER JOIN Endereco E ON E.EnderecoId = C.EnderecoId
+
+SELECT p.BusinessEntityID, p.FirstName, p.LastName, pe.EmailAddress
+FROM Person.Person as P
+INNER JOIN Person.EmailAddress PE on p.BusinessEntityID = pe.BusinessEntityID
+
+-- Queremos os nomes dos produtos e as informações de suas subcategorias 
+SELECT pr.ListPrice,pr.Name,pc.Name
+FROM Production.Product Pr
+INNER JOIN Production.ProductSubcategory PC on PC.ProductSubcategoryID = pr.ProductSubcategoryID
+
+SELECT TOP 10 *
+FROM Person.BusinessEntityAddress BA
+INNER JOIN Person.Address PA ON PA.AddressID = BA.AddressID
+
+SELECT TOP 10 *
+FROM Person.PhoneNumberType
+
+SELECT TOP 10 *
+FROM Person.PersonPhone
+
+SELECT pp.BusinessEntityID, pt.Name, pt.PhoneNumberTypeID, pp.PhoneNumber
+FROM Person.PersonPhone PP
+INNER JOIN Person.PhoneNumberType PT ON PT.PhoneNumberTypeID = pp.PhoneNumberTypeID
+
---
+++ tiposdejoins.sql
@@ -0,0 +1,3 @@
+-- Existem 3 tipos de JOINS;
+-- INNER JOIN, OUTER JOIN E SELF-JOIN
+
